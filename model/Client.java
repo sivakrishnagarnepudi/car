6599def9a4ea60d964244356e66787bedec8dad6
sivakrishnagarnepudi <gsivakrishna007@gmail.com>
2023-11-23 12:22:38 +0530

    car by using open extension principle

---
+++ model/Client.java
@@ -0,0 +1,14 @@
+package model;
+
+public class Client {
+    public static void main (String Args[]){
+        Car req=new Carexample();
+
+        req.carColor("Black");
+        req.carWheel("Alloy wheels");
+
+
+
+    }
+
+}
